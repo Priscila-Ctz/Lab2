404fd3046b2934cba44fb0f20e14bd188922c855
Ana Priscila Cortez Gutierrez <anaprisy@gmail.com>
2020-04-07 13:50:51 -0500

    Select

---
+++ Controllers/VideoController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+using System.Data;
+using MVCLaboratorio.Utilerias;
+
+namespace MVCLaboratorio.Controllers
+{
+    public class VideoController : Controller
+    {
+        //
+        // GET: /Video/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Select()
+        {
+           ViewData["Video"] = BaseHelper.ejecutarConsulta(
+                "SELECT * FROM video", CommandType.Text);
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult Update()
+        {
+            return View();
+        }
+
+        public ActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
---
+++ Views/Video/Select.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" Inherits="System.Web.Mvc.ViewPage<dynamic>" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head runat="server">
+    <title>Select</title>
+</head>
+<body>
+    <a href="/Home"> ↞ Volver al menú 🏡</a>
+    <br />  
+    <h1 class="style2"> <strong>Videos enlistados: </strong> </h1>
+
+        <% foreach (System.Data.DataRow ren in ((System.Data.DataTable) ViewData ["Video"]).Rows)  
+           { %>
+           
+             <ul>
+                 <li>  <%: ren["IDVideo"].ToString()  %> </li>
+                 <li>  <%: ren["titulo"].ToString()  %> </li>
+                 <li>  <%: ren["Url"].ToString()  %> </li>
+                 <li>  <%: ren["reproduccion"].ToString()  %> </li>
+    </ul>
+
+        <% }
+             %>
+</body>
+</html>
